21016bc5c681ff23a05b1d5c1efffcbe6636f1ee
dhruvisha08 <dhruvishab@gmail.com>
2021-09-08 18:02:32 +0530

    commit3

--- src/App.js
+++ src/App.js
@@ -10,12 +10,12 @@ const App = () => {
         <div>
             <Router>
             <Switch>
-                <SignUp/>
-                <Route path="/" exact component={SignUp}/>
-                <Route path="/signUp" component={SignUp}/>
-                <Route path="/signIn" component={SignIn}/>
-                <Route path="/addClient" component={AddClient}/>
-                <Route path="/clientDetails" component={ClientDetails}/>
+                {/* <SignUp/> */}
+                <Route path="dhruvisha08.github.io/ui-react-assignment/" exact component={SignUp}/>
+                <Route path="dhruvisha08.github.io/ui-react-assignment/signUp" component={SignUp}/>
+                <Route path="dhruvisha08.github.io/ui-react-assignment/signIn" component={SignIn}/>
+                <Route path="dhruvisha08.github.io/ui-react-assignment/addClient" component={AddClient}/>
+                <Route path="dhruvisha08.github.io/ui-react-assignment/clientDetails" component={ClientDetails}/>
             </Switch>
             </Router>
         </div>
